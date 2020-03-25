fff71dd1c616eef2f2808adc3150fd23c39b2707
Noah Broyles <noah@javamate.net>
2020-03-25 16:28:56 -0400

    Just tryna fix this ssl issue

--- SmolPlayer.py
+++ SmolPlayer.py
@@ -19,6 +19,11 @@ from os import chdir, getcwd
 from requests import get
 from bs4 import BeautifulSoup
 
+import os.path
+import ssl
+import stat
+import subprocess
+
 class SmolPlayer():
     def __init__(self):
         directory = getcwd()
@@ -166,6 +171,8 @@ class SmolPlayer():
                 self.play()
             except Exception as error:
                 self.threadLock.release()
+                if "url" in error:
+                    installCerts()
                 tkinter.messagebox.showwarning(title='Warning', message=error)
                 self.update()
                 self.play()
@@ -371,6 +378,34 @@ class SmolPlayer():
             self.window.destroy()
         except:
             self.window.destroy()
+
+def installCerts():
+    STAT_0o775 = ( stat.S_IRUSR | stat.S_IWUSR | stat.S_IXUSR
+             | stat.S_IRGRP | stat.S_IWGRP | stat.S_IXGRP
+             | stat.S_IROTH |                stat.S_IXOTH )
+
+    openssl_dir, openssl_cafile = os.path.split(
+        ssl.get_default_verify_paths().openssl_cafile)
+
+    print(" -- pip install --upgrade certifi")
+    subprocess.check_call([sys.executable,
+        "-E", "-s", "-m", "pip", "install", "--upgrade", "certifi"])
+
+    import certifi
+
+    # change working directory to the default SSL directory
+    os.chdir(openssl_dir)
+    relpath_to_certifi_cafile = os.path.relpath(certifi.where())
+    print(" -- removing any existing file or link")
+    try:
+        os.remove(openssl_cafile)
+    except FileNotFoundError:
+        pass
+    print(" -- creating symlink to certifi certificate bundle")
+    os.symlink(relpath_to_certifi_cafile, openssl_cafile)
+    print(" -- setting permissions")
+    os.chmod(openssl_cafile, STAT_0o775)
+    print(" -- update complete")
 
 
 if __name__ == '__main__':
